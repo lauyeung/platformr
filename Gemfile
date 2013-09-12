942e70b54e65ec32b2c69433699d6c856947c404
Louise Au Yeung <louise@eabx.org>
2013-09-12 11:47:42 -0400

    create users almost ready for migration but needs tests

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
+gem 'devise'
+
 group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda'
--- db/migrate/20130911122932_create_users.rb
+++ db/migrate/20130911122932_create_users.rb
@@ -6,11 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :user_photo
-      t.string :account_privacy, null: false
-      t.string :profile_privacy, null: false
-      t.string :comment_privacy, null: false
-      t.string :workout_privacy, null: false
-      t.string :weight_used_privacy, null: false
+      t.string :account_privacy, null: false, default: "private"
 
       t.timestamps
     end

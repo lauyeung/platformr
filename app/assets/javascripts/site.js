b19cc4ebc7ebc0c6e002e8d4362a32a00a2b2b85
Louise Au Yeung <louise@eabx.org>
2013-10-15 20:22:57 -0400

    updated to js link_to

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -7,13 +7,33 @@ $(function() {
     e.stopPropagation();
   });
 
-  $('.add-fields').on('click', function(event) {
-    event.preventDefault();
+  $('#new_workout').on('click', '.add-fields', function(e) {
+    e.preventDefault();
     link = this;
     association = $(this).data('association');
     fields = $(this).data('fields');
     add_fields(link, association, fields);
   });
+
+  // $('.add-fields').click(function(e) {
+  //   e.preventDefault();
+  //   link = this;
+  //   association = $(this).data('association');
+  //   fields = $(this).data('fields');
+  //   add_fields(link, association, fields);
+  // });
+
+  $('#new_workout').on('click', '.remove-fields', function(e) {
+    e.preventDefault();
+    link = this;
+    remove_fields(link);
+  });
+
+  // $('.remove-fields').on('click', function(e) {
+  //   e.preventDefault();
+  //   link = this;
+  //   remove_fields(link);
+  // });
 });
 
 function remove_fields(link) {
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, 'remove_fields(this)')
+    f.hidden_field(:_destroy) + link_to(name, '/', class: 'remove-fields')
+    # link_to_function(name, 'remove_fields(this)')
   end
 
   def link_to_add_fields(name, f, association)

4b81e3445cfb472aa5762e7f21d02edc999f35db
Louise Au Yeung <louise@eabx.org>
2013-10-18 14:53:41 -0400

    add host to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Platformr::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'platformr.heroku.com' }
 end

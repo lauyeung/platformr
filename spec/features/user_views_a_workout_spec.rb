38448d98de807bb518c55834a96a6030b0d7b54b
Louise Au Yeung <louise@eabx.org>
2013-11-02 17:48:35 -0400

    make sure order of combinations stays consistent

--- spec/features/user_views_a_workout_spec.rb
+++ spec/features/user_views_a_workout_spec.rb
@@ -10,14 +10,25 @@ feature 'user views a workout', %Q{
   # * User sees the workouts they have created
 
   let(:user) { FactoryGirl.create(:user) }
-  let(:exercise) { FactoryGirl.create(:exercise, user_id: user.id) }
+  let(:exercise_1) { FactoryGirl.create(:exercise, user_id: user.id) }
+  let(:exercise_2) { FactoryGirl.create(:exercise, user_id: user.id) }
 
   scenario 'user views a workout' do
     sign_in_as(user)
-    create_a_workout(exercise)
+    create_a_workout(exercise_1)
     visit workout_path(Workout.last)
     expect(page).to have_content(Workout.last.workout_date)
   end
+
+  # scenario 'user views a workout with 2 combinations' do
+  #   workout = FactoryGirl.create(:workout, number_of_combinations: 2, user_id: user.id)
+  #   workout.combinations.first.supersets.first.exercise_sets.first.exercise = exercise_1
+  #   workout.combinations.last.supersets.first.exercise_sets.first.exercise = exercise_2
+  #   workout.save!
+  #   sign_in_as(user)
+  #   visit workout_path(Workout.last)
+  #   save_and_open_page
+  # end
 
   include PlatformrTestHelpers
 

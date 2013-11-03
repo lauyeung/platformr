5460243ab6ecaa94636186b6bcec1dee922ebb14
Louise Au Yeung <louise@eabx.org>
2013-11-03 07:36:31 -0500

    remove unneeded test helper

--- spec/support/platformr_test_helpers.rb
+++ spec/support/platformr_test_helpers.rb
@@ -21,27 +21,4 @@ module PlatformrTestHelpers
     click_button 'Create Workout'
   end
 
-  # def create_a_workout_with_two_exercises(exercise_1, exercise_2)
-  #   visit new_workout_path
-  #   select '2013', from: 'workout[workout_date(1i)]'
-  #   select 'October', from: 'workout[workout_date(2i)]'
-  #   select '2', from: 'workout[workout_date(3i)]'
-  #   within('.combination') do
-  #     fill_in 'Sets', with: '5', :match => :prefer_exact
-  #     fill_in 'Weight', with: '50'
-  #     select exercise_1.name, from: 'Exercise'
-  #     fill_in 'Reps', with: '2'
-  #   end
-  #   click_link 'Add a Combination'
-  #   click_link 'Add a Complex'
-  #   click_link 'Add an Exercise'
-  #   within('.combination:nth-child(3)') do
-  #     fill_in 'Sets', with: '5', :match => :prefer_exact
-  #     fill_in 'Weight', with: '50'
-  #     select exercise_2.name, from: 'Exercise'
-  #     fill_in 'Reps', with: '2'
-  #   end
-  #   click_button 'Create Workout'
-  # end
-
 end
